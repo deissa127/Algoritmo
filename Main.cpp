6d903dffaaa4c65c4e9d1979cf8fa9d28da2925d
Hadassa dos Santos Medeiros <Hadassadosmedeiros@gmail.com>
2026-04-23 14:21:56 +0000

    atividade 3 - notação big-oh

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,30 @@
 /*3. Escreva um algoritmo que leia um array A de n números inteiros e retorne a maior diferença de 2 (dois) elementos
 consecutivos de A.*/
 
-#include <iostream>
+#include <iostream> // habilita cin e cout
+#include <climits> // habilita INT_MIN = menor número possível inteiro
 
-int main (){
+int main() {
     int n;
-     std::cin >> n;
+    std::cout << "Digite o tamanho do array: ";
+    std::cin >> n;
 
+    int A[n];
+    std::cout << "Digite os " << n << " elementos:\n";
+    for (int i = 0; i < n; i++) {
+        std::cin >> A[i];
+    }
 
+    int maiorSoma = INT_MIN;
 
+    for (int i = 0; i < n - 1; i++) {
+        int soma = A[i] + A[i + 1];
+        if (soma > maiorSoma) {
+            maiorSoma = soma;
+        }
+    }
 
+    std::cout << "Maior soma de 2 elementos consecutivos: " << maiorSoma << std::endl;
 
-
-     return 0;
+    return 0;
 }
